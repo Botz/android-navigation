096f3fe7c86654f163fd2a3ae8da76f367a7dfb4
Botz <andreas.botzler@gmail.de>
2018-05-10 17:41:17 +0200

    Initial commit

---
+++ app/src/main/java/de/dabotz/navigation/DetailFragment.kt
@@ -0,0 +1,32 @@
+package de.dabotz.navigation
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DetailFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class DetailFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = DetailFragment()
+    }
+}
---
+++ app/src/main/java/de/dabotz/navigation/ListFragment.kt
@@ -0,0 +1,37 @@
+package de.dabotz.navigation
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_list.*
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class ListFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        detailButton.setOnClickListener {
+            findNavController().navigate(R.id.action_list_to_detail)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = ListFragment()
+    }
+}
